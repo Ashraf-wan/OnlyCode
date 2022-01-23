b47f84217f72204967e2739f368b945bc7d2c54f
Wan-Ashraf <hackeranimate@gmail.com>
2022-01-23 18:10:13 +0800

    some change

---
+++ app.js
@@ -0,0 +1,14 @@
+const https = require('https');
+
+const hostname = '192.168.1.105';
+const port = 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
